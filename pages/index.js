eb0a393dba2e4fbe24c678c0374bf4f5d9e83e94
oniromanciaUU <patrick.carroll@ub.uu.se>
2024-03-07 14:39:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,24 @@
-import Head from "next/head";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
-import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Copy the below text</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="rundll32 printui.dll,PrintUIEntry /y /in /n \\edp-uu-prn01.user.uu.se\eduPrint-UU" />
-        <hr />
+        <Header title="Copy the below text" />
         <p className="description">
           rundll32 printui.dll,PrintUIEntry /y /in /n \\edp-uu-prn01.user.uu.se\eduPrint-UU
 
-
-
         </p>
-        <FeedbackForm />
-        <JokeBlock />
       </main>
+
       <Footer />
     </div>
-  );
+  )
 }
